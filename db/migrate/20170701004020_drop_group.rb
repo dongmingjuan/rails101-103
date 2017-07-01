8012dfc13a288ed4a3df85b6721e3eb3883a5418
dongmingjuan <d387918156@gmail.com>
2017-07-01 08:44:05 +0800

    implement groups#create

---
+++ db/migrate/20170701004020_drop_group.rb
@@ -0,0 +1,5 @@
+class DropGroup < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :groups
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701002831) do
+ActiveRecord::Schema.define(version: 20170701004107) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
-    t.text     "descriptin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
